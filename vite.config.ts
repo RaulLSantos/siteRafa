a14131ec19379e62b850c566c92138bbad21da4b
Raul Santos <raul.morais13@gmail.com>
2026-05-14 23:36:05 -0300

    vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
-import { defineConfig } from "@lovable.dev/vite-tanstack-config";
+﻿import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
 
 // GitHub Pages configuration
 // The base path should match your repository name if it's a user/org pages site
 // For user/org pages (https://username.github.io/), use "/" 
 // For project pages (https://username.github.io/law-ethos-space/), use "/law-ethos-space/"
 export default defineConfig({
-  base: "/law-ethos-space/",
+  base: "/siteRafa/",
   tanstackStart: {
     server: { entry: "server" },
   },
